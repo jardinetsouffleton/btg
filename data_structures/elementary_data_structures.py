2c48d7067aa352e4f9379d0f633ca6c9de80da67
Léo Boisvert <leo.boisvert@hotmail.ca>
2021-01-07 20:22:29 -0500

    added binary tree

--- data_structures/elementary_data_structures.py
+++ data_structures/elementary_data_structures.py
@@ -24,7 +24,7 @@ class Queue:
 #### LINKED LIST ####
 #####################
 
-class Node:
+class ListNode:
     def __init__(self, key):
         self.key = key  
         self.next = None
@@ -51,3 +51,49 @@ class DoubleLinkedList:
     def delete(self, x):
         x.prev = x.next
         x.next = x.prev
+
+#####################
+#### ROOTED TREES ###
+#####################
+
+class BinaryTree:
+    def __init__(self, data, left, right):
+        self.data = data
+        self.left = left 
+        self.right = right 
+    
+    def insert(self, data):
+        if self.data:
+            if data < self.data :
+                if self.left is None:
+                    self.left = BinaryTree(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = BinaryTree(data)
+                else:
+                    self.right.insert(data)
+    
+    def get_tree(self):
+        if self.left:
+            self.left.getTree()
+        print(self.data)
+        if self.right:
+            self.right.get_tree()
+
+# class GenericTree:
+#     def __init__(self, data):
+#         self.data = data 
+#         self.left_child = None 
+#         self.right_sibling = None 
+    
+#     def insert(self, data):
+#         if self.data:
+#             if self.left_child is None:
+#                 self.left = GenericTree(data) 
+#             else:
+
+
+
+
